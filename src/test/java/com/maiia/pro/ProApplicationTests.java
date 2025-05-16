c7cd0a6302e0c841a0135ccb2caa33f945faa317
Olivier Tafforin <olivier.tafforin@gmail.com>
2025-05-16 16:55:43 +0200

    Task 2 : Adding/Updating unit tests for ProAvailabilityService and the SpringBoot Application Context

--- src/test/java/com/maiia/pro/ProApplicationTests.java
+++ src/test/java/com/maiia/pro/ProApplicationTests.java
@@ -1,13 +1,36 @@
 package com.maiia.pro;
 
+import com.maiia.pro.controller.ProAppointmentController;
+import com.maiia.pro.controller.ProAvailabilityController;
+import com.maiia.pro.controller.ProPatientController;
+import com.maiia.pro.controller.ProPractitionerController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ProApplicationTests {
 
+	@Autowired
+	private ProAppointmentController appointmentController;
+
+	@Autowired
+	private ProAvailabilityController availabilityController;
+
+	@Autowired
+	private ProPatientController patientController;
+
+	@Autowired
+	private ProPractitionerController practitionerController;
+
+
 	@Test
 	void contextLoads() {
+		assertNotNull(practitionerController);
+		assertNotNull(appointmentController);
+		assertNotNull(patientController);
+		assertNotNull(availabilityController);
 	}
 
 }
